fb7dcde741b882a27a3d469f862b6a3a335b9ad6
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2024-12-30 02:26:34 +0000

    implementa status do database na pagina de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,30 @@
 import database from "infra/database";
 
 async function status(request, response) {
-	const result = await database.query("SELECT 1 + 1 as soma;");
-	console.log(result.rows);
-	response.status(200).json({ status: "its ok. Acima da média" });
+	const databaseName = process.env.POSTGRES_DB;
+	const updatedAt = new Date().toISOString();
+	const resultVersion = await database.query("SHOW server_version;");
+	const resultMaxConnections = await database.query("SHOW max_connections;");
+	const resultAcitiveConnections = await database.query({
+		text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
+		values: [databaseName],
+	});
+
+	const status = {
+		updated_at: updatedAt,
+		dependencies: {
+			database: {
+				type: "postgres",
+				version: resultVersion.rows[0].server_version,
+				max_connections: parseInt(
+					resultMaxConnections.rows[0].max_connections
+				),
+				opened_connections: resultAcitiveConnections.rows[0].count,
+			},
+		},
+	};
+
+	response.status(200).json(status);
 }
 
 export default status;
