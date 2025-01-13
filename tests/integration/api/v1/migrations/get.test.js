bd4b30adc5a3e4d8a4197230af43940e4bed41d7
Bruno Nogueira Fernandes <bruno.tere@gmail.com>
2025-01-13 01:28:53 +0000

    test(rename all descriptions to use new pattern): rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -6,24 +6,38 @@ beforeAll(async () => {
 	await database.query("drop schema public cascade; create schema public;");
 });
 
-test("GET to api/v1/migrations should return 200", async () => {
-	const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
-		method: "GET",
+describe("GET /api/v1/migrations", () => {
+	describe("Anonumous User", () => {
+		describe("Running pending Migrations", () => {
+			test("For the first time", async () => {
+				const response = await fetch(
+					"http://localhost:3000/api/v1/migrations",
+					{
+						method: "GET",
+					},
+				);
+
+				expect(response.status).toBe(200);
+				const responseBody = await response.json();
+
+				expect(Array.isArray(responseBody)).toBe(true);
+				expect(responseBody.length).toBeGreaterThan(0);
+			});
+
+			test("For the second time", async () => {
+				const response = await fetch(
+					"http://localhost:3000/api/v1/migrations",
+					{
+						method: "GET",
+					},
+				);
+
+				expect(response.status).toBe(200);
+				const responseBody = await response.json();
+
+				expect(Array.isArray(responseBody)).toBe(true);
+				expect(responseBody.length).toBeGreaterThan(0);
+			});
+		});
 	});
-
-	expect(response1.status).toBe(200);
-	const response1Body = await response1.json();
-
-	expect(Array.isArray(response1Body)).toBe(true);
-	expect(response1Body.length).toBeGreaterThan(0);
-
-	const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
-		method: "GET",
-	});
-
-	expect(response2.status).toBe(200);
-	const response2Body = await response2.json();
-
-	expect(Array.isArray(response2Body)).toBe(true);
-	expect(response2Body.length).toBeGreaterThan(0);
 });
